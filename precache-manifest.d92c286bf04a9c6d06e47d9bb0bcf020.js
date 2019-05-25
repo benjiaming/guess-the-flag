016511ee4d722e26907cec7171a50e5cf565bdfb
benjiaming <ben@blazke.cz>
2019-05-24 20:38:40 -0700

    Updates

---
+++ precache-manifest.d92c286bf04a9c6d06e47d9bb0bcf020.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cde8dd9691261f6d49eee797e4bc821d",
+    "url": "/guess-the-flag/index.html"
+  },
+  {
+    "revision": "64d87a67e019ed9bad7f",
+    "url": "/guess-the-flag/static/css/main.6b089276.chunk.css"
+  },
+  {
+    "revision": "1095e40177978ce70a23",
+    "url": "/guess-the-flag/static/js/2.c2232ace.chunk.js"
+  },
+  {
+    "revision": "64d87a67e019ed9bad7f",
+    "url": "/guess-the-flag/static/js/main.0db7658c.chunk.js"
+  },
+  {
+    "revision": "4d0d3c52faac45b1be86",
+    "url": "/guess-the-flag/static/js/runtime~main.2a5fba1d.js"
+  },
+  {
+    "revision": "41c62db088a71dab8eba6656a59aa980",
+    "url": "/guess-the-flag/static/media/world.41c62db0.jpg"
+  }
+]);
